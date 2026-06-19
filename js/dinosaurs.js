f50df4dcade550807cdf6b4654ffaaf08d4eac82
イノウエ <a23i246nf@ous.jp>
2026-06-19 12:10:21 +0900

    モデル変更2

--- js/dinosaurs.js
+++ js/dinosaurs.js
@@ -35,7 +35,7 @@ window.DINOSAURS = [
     collectionDistance: 3.1,
     collectionYOffset: 0.06,
     collectionRotation: '0 0 0',
-    arFitTarget: 1.25,  // ⭕ AR画面での3Dモデルの大きさ
+    arFitTarget: 10,  // ⭕ AR画面での3Dモデルの大きさ
     arYOffset: 0     // ⭕ AR画面での上下位置
   },
   {
@@ -96,7 +96,7 @@ window.DINOSAURS = [
     collectionDistance: 3.1,
     collectionYOffset: 0.04,
     collectionRotation: '0 0 0',
-    arFitTarget: 1.25,  // ⭕ AR画面での3Dモデルの大きさ
+    arFitTarget: 0.75,  // ⭕ AR画面での3Dモデルの大きさ
     arYOffset: 0     // ⭕ AR画面での上下位置
   }
 ];
