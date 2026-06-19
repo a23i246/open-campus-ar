20ab82ec8d6d3ae77897f4f4fb296bc747e9e50c
イノウエ <a23i246nf@ous.jp>
2026-06-19 10:04:58 +0900

    モデルのサイズ調整1

--- js/dinosaurs.js
+++ js/dinosaurs.js
@@ -81,7 +81,7 @@ window.DINOSAURS = [
     collectionDistance: 3.1,
     collectionYOffset: 0.06,
     collectionRotation: '0 0 0',
-    arFitTarget: 1.25,  // ⭕ AR画面での3Dモデルの大きさ
+    arFitTarget: 5,  // ⭕ AR画面での3Dモデルの大きさ
     arYOffset: 0     // ⭕ AR画面での上下位置
   },
   {
